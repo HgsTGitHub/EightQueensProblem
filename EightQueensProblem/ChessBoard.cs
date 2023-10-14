617486126ace2f8534415b35fc61fd9f2fbb35e5
Hugo Steenhuis <h.steenhuis@aquadis.nl>
2023-10-14 20:14:39 +0200

    Add project files.

---
+++ EightQueensProblem/ChessBoard.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace EightQueens
+{
+    public class ChessBoard
+    {
+        private readonly BitArray _cells;
+        private readonly byte _queenCount;
+        private readonly byte _size;
+        public ChessBoard(byte size, byte queens, BitArray? cells = null)
+        {
+            _size = size;
+            _cells = cells ?? new BitArray(size * size);
+            _queenCount = queens;
+        }
+        public static List<ChessBoard> Solve(byte size)
+        {
+            var board = new ChessBoard(size, 0);
+            var solutions = new List<ChessBoard>();
+            SolveRecursive(size, 0, board, solutions);
+            return solutions;
+        }
+
+        public static void SolveRecursive(byte size, byte column, ChessBoard board, List<ChessBoard> solutions)
+        {
+            if (column >= size)
+            {
+                if (board.QueenCount() == size)
+                {
+                    solutions.Add(board);
+                    return;
+                }
+            }
+            
+            for (byte row = 0; row < size; row++)
+            {
+                var canPlaceQueen = board.TrySetQueen(row, column);
+                if (canPlaceQueen)
+                {
+                    var newSate = board.Clone().SetQueen(row, column);
+                    SolveRecursive(size, (byte)(column+1), newSate, solutions);
+                }
+            }
+        }
+
+        public ChessBoard Clone() => new(_size, _queenCount, (BitArray)_cells.Clone());
+        public void Reset() { _queenCount = 0; _cells = new BitArray(_size * _size); }
+        public byte QueenCount() => _queenCount;
+        public ChessBoard SetQueen(byte column, byte row)
+        {
+            _cells[row * _size + column] = true;
+            _queenCount++;
+            return this;
+        }
+        public bool TrySetQueen(byte column, byte row)
+        {
+            if (_cells[row * _size + column]) return false;
+            //Spiral checks
+            if (_queenCount >= 1)
+            {
+                for (int index = 0; index < _size; index++)
+                {
+                    //Right
+                    if (column + index < _size)
+                    {
+                        var rightIndex = row * _size + (column + index);
+                        if (_cells[rightIndex])
+                        {
+                            return false;
+                        }
+                    }
+                    //Down & Right
+                    if (row + index < _size && column + index < _size)
+                    {
+                        var downRightIndex = (row + index) * _size + (column + index);
+                        if (_cells[downRightIndex])
+                        {
+                            return false;
+                        }
+                    }
+                    //Down
+                    if (row + index < _size)
+                    {
+                        var downIndex = (row + index) * _size + column;
+                        if (_cells[downIndex])
+                        {
+                            return false;
+                        }
+                    }
+                    //Down & Left
+                    if (row + index < _size && column - index >= 0)
+                    {
+                        var downLeftIndex = (row + index) * _size + (column - index);
+                        if (_cells[downLeftIndex])
+                        {
+                            return false;
+                        }
+                    }
+                    //Left
+                    if (column - index >= 0)
+                    {
+                        var leftIndex = row * _size + (column - index);
+                        if (_cells[leftIndex])
+                        {
+                            return false;
+                        }
+                    }
+                    //Up And Left
+                    if (row - index >= 0 && column - index >= 0)
+                    {
+                        var upLeftIndex = (row - index) * _size + (column - index);
+                        if (_cells[upLeftIndex])
+                        {
+                            return false;
+                        }
+                    }
+                    //Up
+                    if (row - index >= 0)
+                    {
+                        var upIndex = (row - index) * _size + column;
+                        if (_cells[upIndex])
+                        {
+                            return false;
+                        }
+                    }
+                    //Up And Right
+                    if (row - index >= 0 && column + index < _size)
+                    {
+                        var upAndRightIndex = (row - index) * _size + (column + index);
+                        if (_cells[upAndRightIndex])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            for (byte r = 0; r < _size; r++)
+            {
+                for (byte c = 0; c < _size; c++)
+                {
+                    var cell = _cells[r * _size + c] ? "Q " : ". ";
+                    builder.Append($"{cell}");
+                }
+                builder.AppendLine();
+            }
+            return builder.ToString();
+        }
+
+    }
+}
---
+++ EightQueensProblem/Program.cs
@@ -0,0 +1,59 @@
+﻿using EightQueens;
+using System.Diagnostics;
+
+namespace EightQueensProblem
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Try walks over the tree of valid board states without physically
+        /// constructing the tree.
+        /// </summary>
+        /// <param name="all">The number of queens, initially.</param>
+        /// <param name="ld">Contains a one for each position attacked along a
+        /// left going diagonal.</param>
+        /// <param name="cols">A bit pattern containing a one for each column
+        /// that is already occupied.</param>
+        /// <param name="rd">Contains a one for each position attacked along a
+        /// right going diagonal.</param>
+        static int Try(int all, int ld, int cols, int rd)
+        {
+            int count = 0;
+            all = ld != 0 ? all : (1 << all) - 1;
+            int poss = ~(ld | cols | rd) & all;
+
+            while (poss != 0)
+            {
+                int bit = poss & -poss;
+                poss -= bit;
+                count += Try(all, (ld | bit) << 1, cols | bit, (rd | bit) >> 1);
+            }
+
+            return count + (cols == all ? 1 : 0);
+        }
+
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //var solutionCount = Try(15, 0, 0, 0);
+            var solutions = ChessBoard.Solve(13);
+            var solutionCount = solutions.Count;
+            stopwatch.Stop();
+            Console.WriteLine($"Found {solutionCount} in solutions in {stopwatch.ElapsedMilliseconds} milliseconds.");
+            Console.ReadLine();
+            if (solutions.Any())
+            {
+                foreach (var solution in solutions)
+                {
+                    Console.WriteLine(solution.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Solutions");
+            }
+            Console.ReadLine();
+        }
+    }
+}
